9381923c901b0114d2eb3d41785d963d8ab9e7df
poi-vrc <77053052+poi-vrc@users.noreply.github.com>
2023-07-16 17:31:17 +0800

    Add applier settings GUI

--- Packages/com.chocopoi.vrc.dressingtools/Runtime/Applier/DTApplierSettings.cs
+++ Packages/com.chocopoi.vrc.dressingtools/Runtime/Applier/DTApplierSettings.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using UnityEditor;
 using UnityEngine;
 
 namespace Chocopoi.DressingTools.Applier
@@ -13,9 +14,29 @@ namespace Chocopoi.DressingTools.Applier
 
         public bool groupDynamics;
 
+        public DTApplierSettings()
+        {
+            removeExistingPrefixSuffix = true;
+            groupBones = true;
+            groupRootObjects = true;
+            groupDynamics = true;
+        }
+
         public virtual bool DrawEditorGUI()
         {
-            return false;
+            var newRemoveExistingPrefixSuffix = EditorGUILayout.ToggleLeft("Remove existing prefixes and suffixes", removeExistingPrefixSuffix);
+            var newGroupBones = EditorGUILayout.ToggleLeft("Group bones", groupBones);
+            var newGroupRootObjects = EditorGUILayout.ToggleLeft("Group root objects", groupRootObjects);
+            var newGroupDynamics = EditorGUILayout.ToggleLeft("Group dynamics", groupDynamics);
+
+            var modified = newRemoveExistingPrefixSuffix != removeExistingPrefixSuffix || newGroupBones != groupBones || newGroupRootObjects != groupRootObjects || newGroupDynamics != groupDynamics;
+
+            removeExistingPrefixSuffix = newRemoveExistingPrefixSuffix;
+            groupBones = newGroupBones;
+            groupRootObjects = newGroupRootObjects;
+            groupDynamics = newGroupDynamics;
+
+            return modified;
         }
     }
 }
