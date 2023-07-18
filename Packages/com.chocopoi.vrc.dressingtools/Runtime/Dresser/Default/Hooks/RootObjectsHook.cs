0af91efd46df0c20d5a1f3a89ba14aeaefa9c628
poi-vrc <77053052+poi-vrc@users.noreply.github.com>
2023-07-18 18:40:50 +0800

    Add armature guessing in RootObjectsHook

--- Packages/com.chocopoi.vrc.dressingtools/Runtime/Dresser/Default/Hooks/RootObjectsHook.cs
+++ Packages/com.chocopoi.vrc.dressingtools/Runtime/Dresser/Default/Hooks/RootObjectsHook.cs
@@ -10,12 +10,25 @@ namespace Chocopoi.DressingTools.Dresser.Default.Hooks
     {
         public bool Evaluate(DTReport report, DTDresserSettings settings, List<DTBoneMapping> boneMappings, List<DTObjectMapping> objectMappings)
         {
+            var armature = DTRuntimeUtils.GuessArmature(settings.targetWearable, settings.wearableArmatureName);
+
+            if (armature == null)
+            {
+                report.LogError(DTDefaultDresser.MessageCode.NoArmatureInWearable, "Could not find armature in wearable.");
+                return false;
+            }
+
+            if (armature.name != settings.wearableArmatureName)
+            {
+                report.LogInfo(DTDefaultDresser.MessageCode.WearableArmatureObjectGuessed, "Wearable armature object guessed.");
+            }
+
             // map all objects in wearable root except the armature
             for (int i = 0; i < settings.targetWearable.transform.childCount; i++)
             {
                 var child = settings.targetWearable.transform.GetChild(i);
 
-                if (child.name != settings.wearableArmatureName)
+                if (child != armature)
                 {
                     objectMappings.Add(new DTObjectMapping()
                     {
