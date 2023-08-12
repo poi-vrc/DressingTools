e86b78bbea9d75d18d0426b1a3259604eed4f7ee
poi-vrc <77053052+poi-vrc@users.noreply.github.com>
2023-08-13 02:04:56 +0800

    style: add license header

--- Packages/com.chocopoi.vrc.dressingtools/Editor/SettingsWindow.cs
+++ Packages/com.chocopoi.vrc.dressingtools/Editor/SettingsWindow.cs
@@ -1,4 +1,21 @@
-﻿using System.Globalization;
+﻿/*
+ * File: SettingsWindow.cs
+ * Project: DressingTools
+ * Created Date: Saturday, July 22nd 2023, 12:36:56 am
+ * Author: chocopoi (poi@chocopoi.com)
+ * -----
+ * Copyright (c) 2023 chocopoi
+ * 
+ * This file is part of DressingTools.
+ * 
+ * DressingTools is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * DressingTools is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with DressingTools. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System.Globalization;
 using UnityEditor;
 using UnityEngine;
 
