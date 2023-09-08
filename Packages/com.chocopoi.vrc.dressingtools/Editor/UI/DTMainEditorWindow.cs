fb00ef375c554009ec759d27b0c1669eea1c640a
poi-vrc <77053052+poi-vrc@users.noreply.github.com>
2023-09-08 12:03:12 +0800

    refactor: separate GameObjectMenu from MainEditor

--- Packages/com.chocopoi.vrc.dressingtools/Editor/UI/DTMainEditorWindow.cs
+++ Packages/com.chocopoi.vrc.dressingtools/Editor/UI/DTMainEditorWindow.cs
@@ -35,10 +35,6 @@ namespace Chocopoi.DressingTools.UI
     {
         private static readonly Localization.I18n t = Localization.I18n.Instance;
 
-        // note that in order for a menu item in "GameObject/" to be propagated to the
-        // hierarchy Create dropdown and hierarchy context menu, it must be grouped with
-        // the other GameObject creation menu items. This can be achieved by setting its priority to 10 
-        private const int MenuItemPriority = 20;
         private MainView _view;
 
         [MenuItem("Tools/chocopoi/DressingTools", false, 0)]
@@ -49,155 +45,18 @@ namespace Chocopoi.DressingTools.UI
             window.Show();
         }
 
-        [MenuItem("GameObject/DressingTools/Auto-setup wearable (Mappings Only)", false, MenuItemPriority)]
-        static void QuickAutoSetup(MenuCommand menuCommand)
-        {
-            if (!(menuCommand.context is GameObject))
-            {
-                return;
-            }
-
-            var wearable = (GameObject)menuCommand.context;
-
-            // find the avatar
-            var avatarTransform = wearable.transform.parent;
-
-            if (avatarTransform == null || !avatarTransform.TryGetComponent(out DTCabinet cabinet))
-            {
-                // no parent or grandparents has the cabinet
-                EditorUtility.DisplayDialog(t._("tool.name"), t._("menu.dialog.msg.avatarNoCabinetAttached"), t._("common.dialog.btn.ok"));
-                return;
-            }
-
-            if (!CabinetConfig.TryDeserialize(cabinet.configJson, out var cabinetConfig))
-            {
-                EditorUtility.DisplayDialog(t._("tool.name"), t._("menu.dialog.msg.unableToLoadCabinetConfig"), t._("common.dialog.btn.ok"));
-                return;
-            }
-
-            var wearableConfig = new WearableConfig();
-            DTEditorUtils.PrepareWearableConfig(wearableConfig, cabinet.avatarGameObject, wearable);
-
-            var armatureName = cabinetConfig.avatarArmatureName;
-
-            // attempt to find wearable armature using avatar armature name
-            var armature = DTEditorUtils.GuessArmature(wearable, armatureName);
-
-            if (armature == null)
-            {
-                // TODO: ask to select a location for move to
-                EditorUtility.DisplayDialog(t._("tool.name"), t._("menu.dialog.msg.unableToAutoDetectWearableArmature"), t._("common.dialog.btn.ok"));
-                return;
-            }
-            else
-            {
-                if (armature.name != armatureName)
-                {
-                    // TODO: show message
-                }
-
-                var dresserSettings = new DefaultDresserSettings()
-                {
-                    targetAvatar = cabinet.avatarGameObject,
-                    targetWearable = wearable,
-                    dynamicsOption = DefaultDresserDynamicsOption.RemoveDynamicsAndUseParentConstraint
-                };
-
-                var dresser = new DefaultDresser();
-                var report = dresser.Execute(dresserSettings, out _);
-
-                if (report.HasLogType(DTReportLogType.Error))
-                {
-                    ReportWindow.ShowWindow(report);
-                    EditorUtility.DisplayDialog(t._("tool.name"), t._("menu.dialog.msg.defaultDresserHasErrors"), t._("common.dialog.btn.ok"));
-                    return;
-                }
-
-                var armatureMappingModule = new ArmatureMappingWearableModuleConfig
-                {
-                    dresserName = typeof(DefaultDresser).FullName,
-                    wearableArmatureName = armature.name,
-                    boneMappingMode = BoneMappingMode.Auto,
-                    boneMappings = null,
-                    serializedDresserConfig = JsonConvert.SerializeObject(dresserSettings),
-                    removeExistingPrefixSuffix = true,
-                    groupBones = true
-                };
-
-                wearableConfig.modules.Add(new WearableModule()
-                {
-                    moduleName = ArmatureMappingWearableModuleProvider.MODULE_IDENTIFIER,
-                    config = armatureMappingModule
-                });
-            }
-
-            DTEditorUtils.AddCabinetWearable(cabinetConfig, cabinet.avatarGameObject, wearableConfig, wearable);
-        }
-
-        [MenuItem("GameObject/DressingTools/Setup wearable with wizard", true, MenuItemPriority)]
-        [MenuItem("GameObject/DressingTools/Auto-setup wearable (Mappings Only)", true, MenuItemPriority)]
-        public static bool ValidateSetupMenus()
-        {
-            // no selected objects
-            if (Selection.objects.Length == 0)
-            {
-                return false;
-            }
-
-            foreach (var obj in Selection.objects)
-            {
-                if (!(obj is GameObject go))
-                {
-                    // not a GameObject
-                    return false;
-                }
-
-                // find the avatar
-                var avatarTransform = go.transform.parent;
-
-                if (avatarTransform == null || !avatarTransform.TryGetComponent(out DTCabinet _))
-                {
-                    // no parent or grandparents has the cabinet
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        [MenuItem("GameObject/DressingTools/Setup wearable with wizard", false, MenuItemPriority)]
-        public static void StartSetupWizard(MenuCommand menuCommand)
-        {
-            if (!(menuCommand.context is GameObject))
-            {
-                return;
-            }
-
-            var wearable = (GameObject)menuCommand.context;
-
-            // find the avatar
-            var avatarTransform = wearable.transform.parent;
-
-            if (avatarTransform == null || !avatarTransform.TryGetComponent(out DTCabinet cabinet))
-            {
-                // no parent or grandparents has the cabinet
-                EditorUtility.DisplayDialog(t._("tool.name"), t._("menu.dialog.msg.avatarNoCabinetAttached"), t._("common.dialog.btn.ok"));
-                return;
-            }
-
-            var window = (DTMainEditorWindow)GetWindow(typeof(DTMainEditorWindow));
-            window.titleContent = new GUIContent(t._("tool.name"));
-            //window.Show();
-            window._view.SelectedTab = 1;
-            window._view.StartSetupWizard(avatarTransform.gameObject, wearable);
-        }
-
         public DTMainEditorWindow()
         {
             _view = new MainView();
         }
 
         public void SelectCabinet(DTCabinet cabinet) => _view.SelectCabinet(cabinet);
+
+        public void StartSetupWizard(GameObject avatarGameObject, GameObject wearableGameObject)
+        {
+            _view.SelectedTab = 1;
+            _view.StartSetupWizard(avatarGameObject, wearableGameObject);
+        }
 
         public void OnEnable()
         {
