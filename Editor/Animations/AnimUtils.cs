5ac431b6410541e941b613c002cc8b6392de648e
poi-vrc <77053052+poi-vrc@users.noreply.github.com>
2024-02-18 12:29:35 +0800

    feat(vrc): skip vrc internal params from scan

--- Editor/Animations/AnimUtils.cs
+++ Editor/Animations/AnimUtils.cs
@@ -10,6 +10,7 @@
  * You should have received a copy of the GNU General Public License along with DressingTools. If not, see <https://www.gnu.org/licenses/>.
  */
 
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 #if DT_VRCSDK3A
@@ -22,6 +23,37 @@ namespace Chocopoi.DressingTools.Animations
     internal static class AnimUtils
     {
 #if DT_VRCSDK3A
+        private static readonly string[] VRCInternalParameters = new string[] {
+            "IsLocal",
+            "Viseme",
+            "Voice",
+            "GestureLeft",
+            "GestureRight",
+            "GestureLeftWeight",
+            "GestureRightWeight",
+            "AngularY",
+            "VelocityX",
+            "VelocityY",
+            "VelocityZ",
+            "VelocityMagnitude",
+            "Upright",
+            "Grounded",
+            "Seated",
+            "AFK",
+            "TrackingType",
+            "VRMode",
+            "MuteSelf",
+            "InStation",
+            "Earmuffs",
+            "IsOnFriendsList",
+            "AvatarVersion",
+            "ScaleModified",
+            "ScaleFactor",
+            "ScaleFactorInverse",
+            "EyeHeightAsMeters",
+            "EyeHeightAsPercent",
+        };
+
         private static void ScanVRCAvatarLayerAnimatorParameters(Dictionary<string, AnimatorControllerParameterType> parameters, VRCAvatarDescriptor.CustomAnimLayer[] customAnimLayers)
         {
             foreach (var customAnimLayer in customAnimLayers)
@@ -29,6 +61,12 @@ namespace Chocopoi.DressingTools.Animations
                 var animator = VRCAnimUtils.GetCustomAnimLayerAnimator(customAnimLayer);
                 foreach (var animParam in animator.parameters)
                 {
+                    if (Array.IndexOf(VRCInternalParameters, animParam.name) != -1)
+                    {
+                        // skip vrc internal parameters
+                        continue;
+                    }
+
                     if (parameters.TryGetValue(animParam.name, out var type))
                     {
                         if (type != animParam.type)
