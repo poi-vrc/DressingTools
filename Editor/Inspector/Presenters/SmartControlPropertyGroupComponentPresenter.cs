3c4371a4edad31dc0fe99a44c190c52fddfb624b
poi-vrc <77053052+poi-vrc@users.noreply.github.com>
2024-04-20 23:32:19 +0800

    feat: smartcontrol enable object reference types

--- Editor/Inspector/Presenters/SmartControlPropertyGroupComponentPresenter.cs
+++ Editor/Inspector/Presenters/SmartControlPropertyGroupComponentPresenter.cs
@@ -14,7 +14,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Chocopoi.DressingFramework.Localization;
 using Chocopoi.DressingTools.Animations;
 using Chocopoi.DressingTools.Components.Animations;
 using Chocopoi.DressingTools.Inspector.Views;
@@ -27,16 +26,16 @@ namespace Chocopoi.DressingTools.UI.Presenters
     internal class SmartControlPropertyGroupComponentPresenter
     {
         private readonly ISmartControlPropertyGroupComponentView _view;
-        private readonly Dictionary<string, object> _blendshapes;
-        private readonly Dictionary<string, object> _materialProperties;
-        private readonly Dictionary<string, object> _genericProperties;
+        private readonly Dictionary<string, Tuple<Type, object>> _blendshapes;
+        private readonly Dictionary<string, Tuple<Type, object>> _materialProperties;
+        private readonly Dictionary<string, Tuple<Type, object>> _genericProperties;
 
         public SmartControlPropertyGroupComponentPresenter(ISmartControlPropertyGroupComponentView view)
         {
             _view = view;
-            _blendshapes = new Dictionary<string, object>();
-            _materialProperties = new Dictionary<string, object>();
-            _genericProperties = new Dictionary<string, object>();
+            _blendshapes = new Dictionary<string, Tuple<Type, object>>();
+            _materialProperties = new Dictionary<string, Tuple<Type, object>>();
+            _genericProperties = new Dictionary<string, Tuple<Type, object>>();
             SubscribeEvents();
         }
 
@@ -89,7 +88,7 @@ namespace Chocopoi.DressingTools.UI.Presenters
 
         private void OnChangeProperty(string key, object val)
         {
-            SetTargetPropertyValue(key, val);
+            SetOrAddTargetPropertyValue(key, val.GetType(), val);
             _view.Properties[key].value = val;
             _view.RaisePropertiesChangedEvent();
         }
@@ -104,12 +103,11 @@ namespace Chocopoi.DressingTools.UI.Presenters
 
         private void UpdateMaterialShaderProperties(Material material, string path)
         {
-            ForEachShaderProperty(material, (name, val) =>
+            ForEachShaderProperty(material, (name, type, val) =>
             {
                 // the val here is used for determining type only
                 var shaderPath = $"{path}.{name}";
-                var type = val.GetType();
-                if (val is Object)
+                if (type == typeof(Object) || type.IsSubclassOf(typeof(Object)))
                 {
                     var propVal = GetPropertyValue(shaderPath);
                     if (propVal == null)
@@ -143,9 +141,9 @@ namespace Chocopoi.DressingTools.UI.Presenters
                     var newObj = Activator.CreateInstance(type);
                     var foundAll = true;
 
-                    ForEachPublicField(val, (fieldName, fieldVal) =>
+                    ForEachPublicField(type, newObj, (fieldName, fieldType, fieldVal) =>
                     {
-                        if (!(fieldVal is float))
+                        if (fieldType != typeof(float))
                         {
                             return;
                         }
@@ -176,11 +174,16 @@ namespace Chocopoi.DressingTools.UI.Presenters
         {
             for (var i = 0; i < rr.sharedMaterials.Length; i++)
             {
+                var material = rr.sharedMaterials[i];
+                if (material == null)
+                {
+                    continue;
+                }
                 if (i == 0)
                 {
-                    UpdateMaterialShaderProperties(rr.sharedMaterials[i], "material");
+                    UpdateMaterialShaderProperties(material, "material");
                 }
-                UpdateMaterialShaderProperties(rr.sharedMaterials[i], $"materials[{i}]");
+                UpdateMaterialShaderProperties(material, $"materials[{i}]");
             }
         }
 
@@ -211,17 +214,26 @@ namespace Chocopoi.DressingTools.UI.Presenters
 
         private void UpdateGenericProperties()
         {
-            ForEachSerializedObjectProperty(_view.TargetComponent, (path, val) =>
+            ForEachSerializedObjectProperty(_view.TargetComponent, (path, type, val) =>
             {
                 var propVal = GetPropertyValue(path);
                 if (propVal == null)
                 {
                     return;
                 }
+                object obj = null;
+                if (type.IsPrimitive)
+                {
+                    obj = propVal.Value;
+                }
+                else if (type == typeof(Object) || type.IsSubclassOf(typeof(Object)))
+                {
+                    obj = propVal.ValueObjectReference;
+                }
                 _view.Properties.Add(path, new SmartControlComponentViewPropertyValue()
                 {
-                    type = val.GetType(),
-                    value = val
+                    type = type,
+                    value = obj
                 });
             });
         }
@@ -260,12 +272,25 @@ namespace Chocopoi.DressingTools.UI.Presenters
             }
         }
 
-        private static void ForEachPublicField(object obj, Action<string, object> func)
+        private static void ForEachPublicField(Type type, object obj, Action<string, Type, object> func)
         {
-            var fields = obj.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetField | BindingFlags.SetField);
+            var fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetField | BindingFlags.SetField);
             foreach (var field in fields)
             {
-                func(field.Name, field.GetValue(obj));
+                object val;
+                if (obj != null)
+                {
+                    val = field.GetValue(obj);
+                }
+                else if (type.IsValueType)
+                {
+                    val = Activator.CreateInstance(type);
+                }
+                else
+                {
+                    val = null;
+                }
+                func(field.Name, field.FieldType, val);
             }
         }
 
@@ -284,19 +309,6 @@ namespace Chocopoi.DressingTools.UI.Presenters
             return propVals.FirstOrDefault();
         }
 
-        private void SetOrAddTargetPropertyValue(string name, float value, Object objRef)
-        {
-            var propVal = GetPropertyValue(name);
-            if (propVal == null)
-            {
-                propVal = new DTSmartControl.PropertyGroup.PropertyValue();
-                _view.TargetPropertyValues.Add(propVal);
-            }
-            propVal.Name = name;
-            propVal.Value = value;
-            propVal.ValueObjectReference = objRef;
-        }
-
         private void SetOrAddTargetPropertyFromToValue(string name, float fromValue, float toValue)
         {
             var propVal = GetPropertyValue(name);
@@ -310,12 +322,11 @@ namespace Chocopoi.DressingTools.UI.Presenters
             propVal.ToValue = toValue;
         }
 
-        private void SetTargetPropertyValue(string name, object value)
+        private void SetOrAddTargetPropertyValue(string name, Type type, object value)
         {
-            var type = value.GetType();
-            if (value is Object o)
+            if (type == typeof(Object) || type.IsSubclassOf(typeof(Object)))
             {
-                SetOrAddTargetPropertyValue(name, 0.0f, o);
+                SetOrAddTargetPropertyValue(name, 0.0f, (Object)value);
             }
             else if (type.IsPrimitive)
             {
@@ -323,12 +334,25 @@ namespace Chocopoi.DressingTools.UI.Presenters
             }
             else
             {
-                ForEachPublicField(value, (fieldName, fieldVal) =>
+                ForEachPublicField(type, value, (fieldName, fieldType, fieldVal) =>
                 {
-                    if (!(fieldVal is float f)) return;
-                    SetOrAddTargetPropertyValue($"{name}.{fieldName}", f, null);
+                    if (fieldType != typeof(float)) return;
+                    SetOrAddTargetPropertyValue($"{name}.{fieldName}", (float)fieldVal, null);
                 });
             }
+        }
+
+        private void SetOrAddTargetPropertyValue(string name, float value, Object objRef)
+        {
+            var propVal = GetPropertyValue(name);
+            if (propVal == null)
+            {
+                propVal = new DTSmartControl.PropertyGroup.PropertyValue();
+                _view.TargetPropertyValues.Add(propVal);
+            }
+            propVal.Name = name;
+            propVal.Value = value;
+            propVal.ValueObjectReference = objRef;
         }
 
         private void RefreshSearchAndProperties()
@@ -340,7 +364,7 @@ namespace Chocopoi.DressingTools.UI.Presenters
 
         private void OnAddProperty(string name, object value)
         {
-            SetTargetPropertyValue(name, value);
+            SetOrAddTargetPropertyValue(name, value.GetType(), value);
             if (value is float)
             {
                 SetOrAddTargetPropertyFromToValue(name, 0, 100);
@@ -359,17 +383,20 @@ namespace Chocopoi.DressingTools.UI.Presenters
             PerformSearch();
         }
 
-        private static bool CheckIfHasExistingProperty(List<string> existingNames, string name, object value)
+        private static bool CheckIfHasExistingProperty(List<string> existingNames, string name, Type type, object value)
         {
-            var type = value.GetType();
-            if (type == typeof(Object) || type.IsPrimitive)
+            if (type == typeof(Object) || type.IsSubclassOf(typeof(Object)) || type.IsPrimitive)
             {
                 return existingNames.Contains(name);
             }
             else
             {
+                if (value == null)
+                {
+                    return false;
+                }
                 var foundAll = true;
-                ForEachPublicField(value, (fieldName, fieldVal) =>
+                ForEachPublicField(type, value, (fieldName, fieldType, fieldVal) =>
                 {
                     foundAll &= existingNames.Contains($"{name}.{fieldName}");
                 });
@@ -422,7 +449,7 @@ namespace Chocopoi.DressingTools.UI.Presenters
         {
             _view.SearchResults.Clear();
 
-            Dictionary<string, object> source;
+            Dictionary<string, Tuple<Type, object>> source;
             if (_view.SearchResultMode == SmartControlComponentViewResultMode.Blendshapes)
             {
                 source = _blendshapes;
@@ -446,16 +473,17 @@ namespace Chocopoi.DressingTools.UI.Presenters
             {
                 existingPropertyNames.Add(propVal.Name);
             }
-            results.RemoveAll(kvp => CheckIfHasExistingProperty(existingPropertyNames, kvp.Key, kvp.Value));
+            results.RemoveAll(kvp => CheckIfHasExistingProperty(existingPropertyNames, kvp.Key, kvp.Value.Item1, kvp.Value.Item2));
+
             if (_view.ControlType == 1)
             {
                 // for motion time, remove all non-float properties
-                results.RemoveAll(kvp => kvp.Value.GetType() != typeof(float));
+                results.RemoveAll(kvp => kvp.Value.Item1 != typeof(float));
             }
 
             if (!string.IsNullOrEmpty(_view.SearchQuery))
             {
-                var editDistances = new Dictionary<KeyValuePair<string, object>, int>();
+                var editDistances = new Dictionary<KeyValuePair<string, Tuple<Type, object>>, int>();
                 foreach (var kvp in source)
                 {
                     editDistances[kvp] = CalculateLevenshteinDistance(kvp.Key, _view.SearchQuery);
@@ -489,12 +517,12 @@ namespace Chocopoi.DressingTools.UI.Presenters
             {
                 var name = smr.sharedMesh.GetBlendShapeName(i);
                 var value = smr.GetBlendShapeWeight(i);
-                _blendshapes[$"blendShape.{name}"] = value;
+                _blendshapes[$"blendShape.{name}"] = new Tuple<Type, object>(typeof(float), value);
             }
             _view.HasBlendshapes = _blendshapes.Count > 0;
         }
 
-        private static void ForEachSerializedObjectProperty(Object obj, Action<string, object> func)
+        private static void ForEachSerializedObjectProperty(Object obj, Action<string, Type, object> func)
         {
             var so = new SerializedObject(obj);
             so.Update();
@@ -502,41 +530,53 @@ namespace Chocopoi.DressingTools.UI.Presenters
             prop.NextVisible(enterChildren: true);
             while (prop.NextVisible(enterChildren: false))
             {
-                object value = SmartControlUtils.GetSerializedPropertyValue(prop);
-                if (value == null)
+                if (prop.isArray)
                 {
-                    continue;
+                    for (var i = 0; i < prop.arraySize; i++)
+                    {
+                        var arrayProp = prop.GetArrayElementAtIndex(i);
+                        if (!SmartControlUtils.TryGetSerializedPropertyValue(arrayProp, out var type, out var value))
+                        {
+                            continue;
+                        }
+                        func(arrayProp.propertyPath, type, value);
+                    }
                 }
-                func(prop.propertyPath, value);
+                else
+                {
+                    if (!SmartControlUtils.TryGetSerializedPropertyValue(prop, out var type, out var value))
+                    {
+                        continue;
+                    }
+                    func(prop.propertyPath, type, value);
+                }
             }
         }
 
-        private static void ForEachShaderProperty(Material material, Action<string, object> func)
+        private static void ForEachShaderProperty(Material material, Action<string, Type, object> func)
         {
             var shader = material.shader;
             var len = shader.GetPropertyCount();
             for (var i = 0; i < len; i++)
             {
                 var name = shader.GetPropertyName(i);
-                var val = SmartControlUtils.GetShaderProperty(material, i);
-                if (val == null)
+                if (!SmartControlUtils.TryGetShaderProperty(material, i, out var type, out var val))
                 {
                     continue;
                 }
-                func(name, val);
+                func(name, type, val);
             }
         }
 
         private void SearchMaterial(Material material, string prefix)
         {
-            _materialProperties[prefix] = material;
-            ForEachShaderProperty(material, (name, val) =>
+            ForEachShaderProperty(material, (name, type, val) =>
             {
-                _materialProperties[$"{prefix}.{name}"] = val;
+                _materialProperties[$"{prefix}.{name}"] = new Tuple<Type, object>(type, val);
             });
-            ForEachSerializedObjectProperty(material, (path, val) =>
+            ForEachSerializedObjectProperty(material, (path, type, val) =>
             {
-                _materialProperties[$"{prefix}.{path}"] = val;
+                _materialProperties[$"{prefix}.{path}"] = new Tuple<Type, object>(type, val);
             });
         }
 
@@ -545,6 +585,10 @@ namespace Chocopoi.DressingTools.UI.Presenters
             for (var i = 0; i < rr.sharedMaterials.Length; i++)
             {
                 var material = rr.sharedMaterials[i];
+                if (material == null)
+                {
+                    continue;
+                }
                 if (i == 0)
                 {
                     SearchMaterial(material, "material");
@@ -556,9 +600,9 @@ namespace Chocopoi.DressingTools.UI.Presenters
 
         private void SearchGenericProperties(Component comp)
         {
-            ForEachSerializedObjectProperty(comp, (path, val) =>
+            ForEachSerializedObjectProperty(comp, (path, type, val) =>
             {
-                _genericProperties[path] = val;
+                _genericProperties[path] = new Tuple<Type, object>(type, val);
             });
             _view.HasGenericProperties = _genericProperties.Count > 0;
         }
