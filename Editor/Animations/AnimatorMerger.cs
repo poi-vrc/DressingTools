efa155019fd79d967c7f0e0e29200af35dfa2271
poi-vrc <77053052+poi-vrc@users.noreply.github.com>
2024-04-20 21:25:47 +0800

    feat,refactor: manipulate animator component

---
+++ Editor/Animations/AnimatorMerger.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Chocopoi.DressingFramework;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+using Object = UnityEngine.Object;
+#if DT_VRCSDK3A
+using Chocopoi.DressingFramework.Animations.VRChat;
+#endif
+
+namespace Chocopoi.DressingTools.Animations
+{
+    /// <summary>
+    /// Temporarily made a copy here from AvatarLib
+    /// </summary>
+    internal class AnimatorMerger
+    {
+        public enum WriteDefaultsMode
+        {
+            DoNothing = 0,
+            On = 1,
+            Off = 2
+        }
+
+        private readonly Context _ctx;
+        private readonly AnimatorController _mergedAnimator;
+        private readonly Dictionary<Tuple<string, AnimatorStateMachine>, AnimatorStateMachine> _stateMachineCache;
+        private readonly Dictionary<string, AnimatorControllerParameter> _parameters;
+        private readonly Dictionary<string, AnimatorControllerLayer> _layers;
+        private readonly Dictionary<string, int> _layerNameDuplicateCount;
+
+        public AnimatorMerger(Context ctx, string name)
+        {
+            _ctx = ctx;
+            _mergedAnimator = new AnimatorController();
+            ctx.CreateUniqueAsset(_mergedAnimator, name);
+            _stateMachineCache = new Dictionary<Tuple<string, AnimatorStateMachine>, AnimatorStateMachine>();
+            _parameters = new Dictionary<string, AnimatorControllerParameter>();
+            _layers = new Dictionary<string, AnimatorControllerLayer>();
+            _layerNameDuplicateCount = new Dictionary<string, int>();
+        }
+
+        public void AddAnimator(string rebasePath, AnimatorController animator, WriteDefaultsMode writeDefaultsMode = WriteDefaultsMode.DoNothing)
+        {
+            // deep copy parameters
+            DeepCopyParameters(animator);
+
+            // deep copy layers
+            var layers = animator.layers;
+            for (var i = 0; i < layers.Length; i++)
+            {
+                var originalLayer = layers[i];
+                var weight = i == 0 ? 1 : originalLayer.defaultWeight;
+
+                var copyCache = new Dictionary<Object, Object>();
+
+                var newLayerName = originalLayer.name;
+                if (_layers.ContainsKey(originalLayer.name))
+                {
+                    if (!_layerNameDuplicateCount.TryGetValue(originalLayer.name, out var count))
+                    {
+                        count = 1;
+                    }
+                    newLayerName += "_" + count;
+                    _layerNameDuplicateCount[originalLayer.name] = ++count;
+                }
+
+                // create copy
+                var newLayer = new AnimatorControllerLayer()
+                {
+                    name = newLayerName,
+                    defaultWeight = weight,
+                    blendingMode = originalLayer.blendingMode,
+                    iKPass = originalLayer.iKPass,
+                    avatarMask = originalLayer.avatarMask,
+                    syncedLayerIndex = originalLayer.syncedLayerIndex,
+                    syncedLayerAffectsTiming = originalLayer.syncedLayerAffectsTiming,
+                    stateMachine = DeepCopyStateMachineWithRebasing(rebasePath, originalLayer.stateMachine, copyCache),
+                };
+
+                HandleWriteDefaults(newLayer.stateMachine, writeDefaultsMode);
+
+                if (originalLayer.syncedLayerIndex != -1 && originalLayer.syncedLayerIndex >= 0 && originalLayer.syncedLayerIndex < layers.Length)
+                {
+                    HandleSyncedLayer(originalLayer, layers[originalLayer.syncedLayerIndex], newLayer, copyCache);
+                }
+
+                _layers.Add(newLayer.name, newLayer);
+            }
+        }
+
+        public AnimatorController Merge()
+        {
+            _mergedAnimator.layers = _layers.Values.ToArray();
+            _mergedAnimator.parameters = _parameters.Values.ToArray();
+            EditorUtility.SetDirty(_mergedAnimator);
+            return _mergedAnimator;
+        }
+
+        private void HandleSyncedLayer(AnimatorControllerLayer originalLayer, AnimatorControllerLayer baseLayer, AnimatorControllerLayer newLayer, Dictionary<Object, Object> copyCache)
+        {
+            var it = WalkStateMachine(baseLayer.stateMachine);
+            foreach (var state in it)
+            {
+                var animState = (AnimatorState)copyCache[state];
+
+                // copy override motion
+                var motion = originalLayer.GetOverrideMotion(state);
+                if (motion != null)
+                {
+                    newLayer.SetOverrideMotion(animState, motion);
+                }
+
+                // copy override behaviours
+                var behaviours = originalLayer.GetOverrideBehaviours(state);
+                if (behaviours != null)
+                {
+                    var copy = new StateMachineBehaviour[behaviours.Length];
+                    behaviours.CopyTo(copy, 0);
+
+                    for (var i = 0; i < copy.Length; i++)
+                    {
+                        behaviours[i] = GenericDeepCopy(behaviours[i]);
+                        HandleAnimatorBehaviour(behaviours[i]);
+                    }
+
+                    newLayer.SetOverrideBehaviours(animState, copy);
+                }
+            }
+            newLayer.syncedLayerIndex += _layers.Count;
+        }
+
+        private static string RebaseCurvePath(EditorCurveBinding curveBinding, string rebasePath)
+        {
+            if (rebasePath == "" && curveBinding.type == typeof(Animator))
+            {
+                return "";
+            }
+
+            return curveBinding.path == "" ?
+                rebasePath :
+                (rebasePath + "/" + curveBinding.path);
+        }
+
+        private void HandleWriteDefaults(AnimatorStateMachine stateMachine, WriteDefaultsMode writeDefaultsMode)
+        {
+            if (writeDefaultsMode == WriteDefaultsMode.DoNothing)
+            {
+                return;
+            }
+
+            // iterate all states
+            foreach (var state in stateMachine.states)
+            {
+                state.state.writeDefaultValues = writeDefaultsMode == WriteDefaultsMode.On;
+            }
+
+            // iterate all child state machines
+            foreach (var childStateMachine in stateMachine.stateMachines)
+            {
+                HandleWriteDefaults(childStateMachine.stateMachine, writeDefaultsMode);
+            }
+        }
+
+        private AnimatorStateMachine DeepCopyStateMachineWithRebasing(string rebasePath, AnimatorStateMachine stateMachine, Dictionary<Object, Object> copyCache = null)
+        {
+            if (copyCache == null)
+            {
+                copyCache = new Dictionary<Object, Object>();
+            }
+
+            // attempt to find cache to not copy again
+            var key = new Tuple<string, AnimatorStateMachine>(rebasePath, stateMachine);
+            if (_stateMachineCache.TryGetValue(key, out var cachedStateMachine))
+            {
+                return cachedStateMachine;
+            }
+
+            var newStateMachine = GenericDeepCopy(stateMachine, obj =>
+            {
+                if (!(obj is AnimationClip))
+                {
+                    return null;
+                }
+
+                var anim = (AnimationClip)obj;
+                if (rebasePath == "") return anim;
+#if DT_VRCSDK3A
+                if (VRCAnimUtils.IsProxyAnimation(anim)) return anim;
+#endif
+
+                // create new copy
+                var newAnim = new AnimationClip
+                {
+                    name = anim.name + "_Rebase",
+                    legacy = anim.legacy,
+                    frameRate = anim.frameRate,
+                    localBounds = anim.localBounds,
+                    wrapMode = anim.wrapMode
+                };
+                AnimationUtility.SetAnimationClipSettings(newAnim, AnimationUtility.GetAnimationClipSettings(anim));
+
+                AssetDatabase.AddObjectToAsset(newAnim, _mergedAnimator);
+
+                // object reference curves
+                var objRefBindings = AnimationUtility.GetObjectReferenceCurveBindings(anim);
+                foreach (var objRefBinding in objRefBindings)
+                {
+                    var newObjRefBinding = objRefBinding;
+                    newObjRefBinding.path = RebaseCurvePath(objRefBinding, rebasePath);
+                    AnimationUtility.SetObjectReferenceCurve(newAnim, newObjRefBinding, AnimationUtility.GetObjectReferenceCurve(anim, objRefBinding));
+                }
+
+                // curves
+                var curveBindings = AnimationUtility.GetCurveBindings(anim);
+                foreach (var curveBinding in curveBindings)
+                {
+                    newAnim.SetCurve(RebaseCurvePath(curveBinding, rebasePath), curveBinding.type, curveBinding.propertyName, AnimationUtility.GetEditorCurve(anim, curveBinding));
+                }
+
+                return newAnim;
+            }, copyCache);
+
+            // update animator behaviours
+            var it = WalkStateMachine(stateMachine);
+            foreach (var state in it)
+            {
+                foreach (var behaviour in state.behaviours)
+                {
+                    HandleAnimatorBehaviour(behaviour);
+                }
+            }
+
+            // save to cache
+            _stateMachineCache[key] = newStateMachine;
+            return newStateMachine;
+        }
+
+        private void HandleAnimatorBehaviour(StateMachineBehaviour behaviour)
+        {
+#if DT_VRCSDK3A
+            if (behaviour is VRC.SDK3.Avatars.Components.VRCAnimatorLayerControl control)
+            {
+                // update control layer index
+                control.layer = _layers.Count;
+            }
+#endif
+        }
+
+        private void DeepCopyParameters(AnimatorController animator)
+        {
+            foreach (var param in animator.parameters)
+            {
+                if (_parameters.TryGetValue(param.name, out var existingType))
+                {
+                    if (param.type != existingType.type)
+                    {
+                        // throw exception if type mismatch
+                        _ctx.Report.LogWarn("AnimatorMerger", $"Animator types mismatch for {param.name}: current: {param.type} existing {existingType}, forcing to use float type");
+                        _parameters[param.name].type = AnimatorControllerParameterType.Float;
+                    }
+                }
+                else
+                {
+                    // deep copy parameter
+                    var newParam = new AnimatorControllerParameter
+                    {
+                        name = param.name,
+                        type = param.type,
+                        defaultBool = param.defaultBool,
+                        defaultFloat = param.defaultFloat,
+                        defaultInt = param.defaultInt
+                    };
+                    _parameters.Add(param.name, newParam);
+                }
+            }
+        }
+
+        private static IEnumerable<AnimatorState> WalkStateMachine(AnimatorStateMachine stateMachine, HashSet<AnimatorStateMachine> visitedStateMachines = null)
+        {
+            if (visitedStateMachines == null)
+            {
+                visitedStateMachines = new HashSet<AnimatorStateMachine>();
+            }
+
+            // do not walk visited state machines
+            if (visitedStateMachines.Contains(stateMachine))
+            {
+                yield break;
+            }
+            visitedStateMachines.Add(stateMachine);
+
+            // yield all states
+            foreach (var state in stateMachine.states)
+            {
+                if (state.state == null)
+                {
+                    continue;
+                }
+
+                yield return state.state;
+            }
+
+            // recursive visit other state machines
+            foreach (var childStateMachine in stateMachine.stateMachines)
+            {
+                if (stateMachine.stateMachines == null)
+                {
+                    continue;
+                }
+
+                if (visitedStateMachines.Contains(childStateMachine.stateMachine))
+                {
+                    continue;
+                }
+                visitedStateMachines.Add(childStateMachine.stateMachine);
+
+                // walk the child state machine
+                var states = WalkStateMachine(childStateMachine.stateMachine, visitedStateMachines);
+                foreach (var state in states)
+                {
+                    yield return state;
+                }
+            }
+        }
+
+        // TODO: reuse from DK, probably move to avatarlib with a more generic implementation
+        private T GenericDeepCopy<T>(T originalObject, Func<Object, Object> genericCopyFunc = null, Dictionary<Object, Object> copyCache = null) where T : Object
+        {
+            if (copyCache == null)
+            {
+                copyCache = new Dictionary<Object, Object>();
+            }
+
+            if (originalObject == null)
+            {
+                return null;
+            }
+
+            var originalObjectType = originalObject.GetType();
+
+            // do not copy these types and return original
+            if (originalObject is MonoScript ||
+                originalObject is ScriptableObject ||
+                originalObject is Texture ||
+                originalObject is Material)
+            {
+                return originalObject;
+            }
+
+            // only copy known types
+            if (!(originalObject is Motion ||
+                originalObject is AnimatorController ||
+                originalObject is AnimatorStateMachine ||
+                originalObject is StateMachineBehaviour ||
+                originalObject is AnimatorState ||
+                originalObject is AnimatorTransitionBase))
+            {
+                throw new Exception(string.Format("Unknown type detected while animator merging: {0}", originalObjectType.FullName));
+            }
+
+            // try obtain from cache
+            if (copyCache.TryGetValue(originalObject, out var obj))
+            {
+                return (T)obj;
+            }
+
+            Object newObj;
+
+            // attempt to copy with generic copy function
+            if (genericCopyFunc != null)
+            {
+                newObj = genericCopyFunc(originalObject);
+                if (newObj != null)
+                {
+                    return (T)newObj;
+                }
+            }
+
+            // initialize a new object in a generic way
+            var constructor = originalObjectType.GetConstructor(System.Type.EmptyTypes);
+            if (constructor != null && !(originalObject is ScriptableObject))
+            {
+                newObj = (T)System.Activator.CreateInstance(originalObjectType);
+                // copy serialized properties
+                EditorUtility.CopySerialized(originalObject, newObj);
+            }
+            else
+            {
+                newObj = Object.Instantiate(originalObject);
+            }
+            copyCache[originalObject] = newObj;
+
+            // save to assets
+            AssetDatabase.AddObjectToAsset(newObj, _mergedAnimator);
+
+            // deep copy serialized properties
+            var serializedObj = new SerializedObject(newObj);
+            var it = serializedObj.GetIterator();
+
+            bool traverseDown = true;
+            while (it.Next(traverseDown))
+            {
+                // reset
+                traverseDown = true;
+
+                if (it.propertyType == SerializedPropertyType.String)
+                {
+                    // disable traversal
+                    traverseDown = false;
+                }
+                else if (it.propertyType == SerializedPropertyType.ObjectReference)
+                {
+                    // recursively perform deep copy
+                    it.objectReferenceValue = GenericDeepCopy(it.objectReferenceValue, genericCopyFunc, copyCache);
+                }
+            }
+
+            // apply changes
+            serializedObj.ApplyModifiedPropertiesWithoutUndo();
+
+            return (T)newObj;
+        }
+    }
+}
---
+++ Editor/Passes/Animations/ManipulateAnimatorPass.cs
@@ -0,0 +1,261 @@
+﻿/*
+ * Copyright (c) 2024 chocopoi
+ * 
+ * This file is part of DressingTools.
+ * 
+ * DressingTools is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * DressingTools is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with DressingTools. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System.Collections.Generic;
+using Chocopoi.AvatarLib.Animations;
+using Chocopoi.DressingFramework;
+using Chocopoi.DressingFramework.Extensibility.Sequencing;
+using Chocopoi.DressingFramework.Localization;
+using Chocopoi.DressingTools.Animations;
+using Chocopoi.DressingTools.Components;
+using Chocopoi.DressingTools.Components.Animations;
+using Chocopoi.DressingTools.Localization;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+using AnimatorMerger = Chocopoi.DressingTools.Animations.AnimatorMerger;
+#if DT_VRCSDK3A
+using Chocopoi.DressingFramework.Animations.VRChat;
+using VRC.SDK3.Avatars.Components;
+#endif
+
+namespace Chocopoi.DressingTools.Passes.Modifiers
+{
+    [ComponentPassFor(typeof(DTBlendshapeSync))]
+    internal class ManipulateAnimatorPass : ComponentPass
+    {
+        public class MessageCode
+        {
+        }
+        private const string LogLabel = "ManipulateAnimatorPass";
+
+        private const string VRCGestureAvatarMaskGuid = "b2b8bad9583e56a46a3e21795e96ad92";
+        private static readonly I18nTranslator t = I18n.ToolTranslator;
+
+        public override BuildConstraint Constraint =>
+            InvokeAtStage(BuildStage.Transpose)
+                .Build();
+
+#if DT_VRCSDK3A
+        private static AnimatorController PrepareVRCAnimLayerTarget(Context ctx, Dictionary<AnimatorController, bool> clonedStatus, Dictionary<AnimatorController, AnimatorMerger.WriteDefaultsMode> writeDefaults, bool matchTargetWriteDefaults, VRCAvatarDescriptor.AnimLayerType animLayerType)
+        {
+            if (!ctx.AvatarGameObject.TryGetComponent<VRCAvatarDescriptor>(out var avatarDesc))
+            {
+                // not a vrc avatar
+                return null;
+            }
+
+            // TODO: becareful this might a default animator which is not cloned + having a same hashcode
+            var layerAnimator = VRCAnimUtils.GetAvatarLayerAnimator(avatarDesc, animLayerType);
+            if (layerAnimator == null)
+            {
+                return null;
+            }
+
+            clonedStatus[layerAnimator] = true;
+            AnimUtils.GetWriteDefaultCounts(layerAnimator, out var onCount, out var offCount);
+            if (!matchTargetWriteDefaults)
+            {
+                writeDefaults[layerAnimator] = AnimatorMerger.WriteDefaultsMode.DoNothing;
+            }
+            else
+            {
+                if (onCount == 0 && offCount > 0)
+                {
+                    writeDefaults[layerAnimator] = AnimatorMerger.WriteDefaultsMode.Off;
+                }
+                else if (onCount > 0 && offCount == 0)
+                {
+                    writeDefaults[layerAnimator] = AnimatorMerger.WriteDefaultsMode.On;
+                }
+                else
+                {
+                    writeDefaults[layerAnimator] = AnimatorMerger.WriteDefaultsMode.DoNothing;
+                }
+            }
+
+            return layerAnimator;
+        }
+
+        // TODO: this function is temporarily until reference tracker is implemented
+        private static VRCAvatarDescriptor.CustomAnimLayer[] GetVRCMergedLayers(Dictionary<AnimatorController, AnimatorMerger> mergers, VRCAvatarDescriptor.CustomAnimLayer[] animLayers)
+        {
+            // create a new array and shallow copy
+            var output = new VRCAvatarDescriptor.CustomAnimLayer[animLayers.Length];
+            animLayers.CopyTo(output, 0);
+
+            for (var i = 0; i < output.Length; i++)
+            {
+                if (!(output[i].animatorController is AnimatorController animCtrl))
+                {
+                    continue;
+                }
+
+                // get the merger of such layer
+                if (mergers.TryGetValue(animCtrl, out var merger))
+                {
+                    output[i].animatorController = merger.Merge();
+                    output[i].isDefault = false;
+
+                    // add avatar mask for default gesture layer
+                    if (output[i].type == VRCAvatarDescriptor.AnimLayerType.Gesture && output[i].isDefault)
+                    {
+                        var maskPath = AssetDatabase.GUIDToAssetPath(VRCGestureAvatarMaskGuid);
+                        if (maskPath == null || maskPath == "")
+                        {
+                            throw new System.Exception("Unable to find VRC gesture avatar mask by GUID!");
+                        }
+                        output[i].mask = AssetDatabase.LoadAssetAtPath<AvatarMask>(maskPath);
+                    }
+                }
+            }
+
+            return output;
+        }
+
+        private void WriteToVRCAvatarDescriptor(Context ctx, Dictionary<AnimatorController, AnimatorMerger> mergers)
+        {
+            if (!ctx.AvatarGameObject.TryGetComponent<VRCAvatarDescriptor>(out var avatarDesc))
+            {
+                // not a vrc avatar
+                return;
+            }
+
+            avatarDesc.customizeAnimationLayers = true;
+
+            // write merge layers
+            avatarDesc.baseAnimationLayers = GetVRCMergedLayers(mergers, avatarDesc.baseAnimationLayers);
+            avatarDesc.specialAnimationLayers = GetVRCMergedLayers(mergers, avatarDesc.specialAnimationLayers);
+        }
+#endif
+
+        private static AnimatorMerger PrepareTargetMerger(Context ctx, Dictionary<AnimatorController, AnimatorMerger> mergers, AnimatorController targetController)
+        {
+            if (mergers.TryGetValue(targetController, out var merger))
+            {
+                return merger;
+            }
+
+            // this is cloned initially
+            merger = mergers[targetController] = new AnimatorMerger(ctx, $"{targetController}_Merged");
+            merger.AddAnimator("", targetController);
+
+            return merger;
+        }
+
+        private static bool TryGetSourceController(Transform avatarRoot, DTManipulateAnimator comp, out Transform relativeRoot, out AnimatorController controller)
+        {
+            relativeRoot = null;
+            controller = null;
+            if (comp.SourceType == DTManipulateAnimator.SourceTypes.AnimatorController)
+            {
+                if (comp.PathMode == DTManipulateAnimator.PathModes.Relative)
+                {
+                    relativeRoot = comp.SourceRelativeRoot != null ? comp.SourceRelativeRoot : comp.transform;
+                }
+                else
+                {
+                    relativeRoot = avatarRoot;
+                }
+                controller = comp.SourceController;
+                return true;
+            }
+            else if (comp.SourceType == DTManipulateAnimator.SourceTypes.Animator)
+            {
+                var animator = comp.SourceAnimator;
+                if (animator == null && !comp.TryGetComponent(out animator))
+                {
+                    return false;
+                }
+
+                if (!(animator.runtimeAnimatorController is AnimatorController animCtrl))
+                {
+                    return false;
+                }
+                relativeRoot = comp.PathMode == DTManipulateAnimator.PathModes.Relative ? animator.transform : avatarRoot;
+                controller = animCtrl;
+                return true;
+            }
+            return false;
+        }
+
+        public override bool Invoke(Context ctx)
+        {
+            var clonedStatus = new Dictionary<AnimatorController, bool>();
+            var mergers = new Dictionary<AnimatorController, AnimatorMerger>();
+            var writeDefaults = new Dictionary<AnimatorController, AnimatorMerger.WriteDefaultsMode>();
+
+            var comps = ctx.AvatarGameObject.GetComponentsInChildren<DTManipulateAnimator>(true);
+            foreach (var comp in comps)
+            {
+                // TODO: not implemented
+                if (comp.TargetType != DTManipulateAnimator.TargetTypes.VRCAnimLayer)
+                {
+                    ctx.Report.LogWarn(LogLabel, "Target types other than VRCAnimLayer are not implemented, skipping");
+                    continue;
+                }
+                if (comp.ManipulateMode != DTManipulateAnimator.ManipulateModes.Add)
+                {
+                    ctx.Report.LogWarn(LogLabel, "Manipulate modes other than Add are not implemented, skipping");
+                    continue;
+                }
+
+                AnimatorController targetController;
+#if DT_VRCSDK3A
+                targetController = PrepareVRCAnimLayerTarget(ctx, clonedStatus, writeDefaults, comp.MatchTargetWriteDefaults, comp.VRCTargetLayer);
+#else
+                targetController = null;
+#endif
+                if (targetController == null)
+                {
+                    ctx.Report.LogWarn(LogLabel, $"Could not prepare target controller for {comp.name}, skipping");
+                    continue;
+                }
+
+                if (!TryGetSourceController(ctx.AvatarGameObject.transform, comp, out var relativeRoot, out var sourceCtrl))
+                {
+                    ctx.Report.LogWarn(LogLabel, $"Could not obtain source controller for {comp.name}, skipping");
+                    continue;
+                }
+
+                var merger = PrepareTargetMerger(ctx, mergers, targetController);
+                var rebasePath = "";
+                if (relativeRoot != ctx.AvatarGameObject.transform)
+                {
+                    rebasePath = AnimationUtils.GetRelativePath(relativeRoot, ctx.AvatarGameObject.transform);
+                }
+
+                merger.AddAnimator(rebasePath, sourceCtrl);
+
+                if (comp.SourceType == DTManipulateAnimator.SourceTypes.Animator &&
+                    comp.RemoveSourceAnimator)
+                {
+                    Object.DestroyImmediate(comp.SourceAnimator);
+                }
+            }
+
+#if DT_VRCSDK3A
+            WriteToVRCAvatarDescriptor(ctx, mergers);
+#endif
+
+            return true;
+        }
+
+        public override bool Invoke(Context ctx, DTBaseComponent component, out List<DTBaseComponent> generatedComponents)
+        {
+            // TODO: caution on GetAvatarLayerAnimator returning default asset which is not cloned
+            Debug.LogWarning("[DressingTools] Component apply on ManipulateAnimatorPass is not implemented yet.");
+            generatedComponents = new List<DTBaseComponent>();
+            return true;
+        }
+    }
+}
