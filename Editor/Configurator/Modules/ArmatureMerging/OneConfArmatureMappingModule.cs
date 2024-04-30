6e2fd7348f1b7480ad8e2be6a1dd593c10abbb97
poi-vrc <77053052+poi-vrc@users.noreply.github.com>
2024-04-30 16:15:27 +0800

    s

--- Editor/Configurator/Modules/ArmatureMerging/OneConfArmatureMappingModule.cs
+++ Editor/Configurator/Modules/ArmatureMerging/OneConfArmatureMappingModule.cs
@@ -13,97 +13,62 @@
 using Chocopoi.AvatarLib.Animations;
 using Chocopoi.DressingTools.Components.OneConf;
 using Chocopoi.DressingTools.OneConf;
-using Chocopoi.DressingTools.OneConf.Cabinet;
-using Chocopoi.DressingTools.OneConf.Serialization;
 using Chocopoi.DressingTools.OneConf.Wearable.Modules.BuiltIn;
 using Chocopoi.DressingTools.UI.Views;
 using UnityEngine;
 
 namespace Chocopoi.DressingTools.Configurator.Modules
 {
-    internal class OneConfArmatureMappingModule : IArmatureMergingModule
+    internal class OneConfArmatureMappingModule : OneConfModuleBase, IArmatureMergingModule
     {
         public Transform TargetArmature
         {
             get
             {
-                if (!_avatarGameObject.TryGetComponent<DTCabinet>(out var cabinetComp) ||
-                    !CabinetConfigUtility.TryDeserialize(cabinetComp.ConfigJson, out var cabinetConfig))
-                {
-                    cabinetConfig = new CabinetConfig();
-                }
-                if (string.IsNullOrEmpty(cabinetConfig.avatarArmatureName))
-                {
-                    return null;
-                }
-                return OneConfUtils.GuessArmature(_avatarGameObject, cabinetConfig.avatarArmatureName);
+                ReadCabinetConfig(out _, out var config);
+                return string.IsNullOrEmpty(config.avatarArmatureName) ?
+                    null :
+                    OneConfUtils.GuessArmature(_avatarGameObject, config.avatarArmatureName);
             }
             set
             {
-                CabinetConfig cabinetConfig;
-                if (_avatarGameObject.TryGetComponent<DTCabinet>(out var cabinetComp))
+                WriteCabinetConfig((comp, config) =>
                 {
-                    if (!CabinetConfigUtility.TryDeserialize(cabinetComp.ConfigJson, out cabinetConfig))
-                    {
-                        cabinetConfig = new CabinetConfig();
-                    }
-                }
-                else
-                {
-                    cabinetComp = _avatarGameObject.AddComponent<DTCabinet>();
-                    cabinetConfig = new CabinetConfig();
-                }
-                cabinetConfig.avatarArmatureName =
-                    value == null ?
-                    "" :
-                    AnimationUtils.GetRelativePath(value.transform, cabinetComp.transform);
-                cabinetComp.ConfigJson = CabinetConfigUtility.Serialize(cabinetConfig);
+                    config.avatarArmatureName =
+                        value == null ?
+                        "" :
+                        AnimationUtils.GetRelativePath(value.transform, comp.transform);
+                });
             }
         }
         public Transform SourceArmature
         {
             get
             {
-                if (!WearableConfigUtility.TryDeserialize(_wearableComp.ConfigJson, out var config))
-                {
-                    return null;
-                }
-                var module = config.FindModuleConfig<ArmatureMappingWearableModuleConfig>();
-                if (module == null || string.IsNullOrEmpty(module.wearableArmatureName))
-                {
-                    return null;
-                }
-                return OneConfUtils.GuessArmature(_wearableComp.gameObject, module.wearableArmatureName);
+                ReadWearableModule<ArmatureMappingWearableModuleConfig>(out var module);
+                if (module == null) return null;
+                return string.IsNullOrEmpty(module.wearableArmatureName) ?
+                    null :
+                    OneConfUtils.GuessArmature(_wearableComp.gameObject, module.wearableArmatureName);
             }
             set
             {
-                if (!WearableConfigUtility.TryDeserialize(_wearableComp.ConfigJson, out var config))
+                WriteWearableModule<ArmatureMappingWearableModuleConfig>(module =>
                 {
-                    return;
-                }
-                var module = config.FindModuleConfig<ArmatureMappingWearableModuleConfig>();
-                if (module == null)
-                {
-                    return;
-                }
-                module.wearableArmatureName =
-                    value == null ?
-                    "" :
-                    AnimationUtils.GetRelativePath(value.transform, _wearableComp.transform);
-                _wearableComp.ConfigJson = WearableConfigUtility.Serialize(config);
+                    module.wearableArmatureName =
+                        value == null ?
+                        "" :
+                        AnimationUtils.GetRelativePath(value.transform, _wearableComp.transform);
+                });
             }
         }
 
-        private readonly GameObject _avatarGameObject;
-        private readonly DTWearable _wearableComp;
-
         public OneConfArmatureMappingModule(GameObject avatarGameObject, DTWearable wearableComp)
+        : base(avatarGameObject, wearableComp)
         {
-            _avatarGameObject = avatarGameObject;
-            _wearableComp = wearableComp;
         }
 
-        public ElementView CreateView()
+        public override ElementView CreateView()
         {
             throw new System.NotImplementedException();
         }
